8da043f039a6e7b1c26cc8804507e20a6ab6e38c
unknown <tenken74@gmail.com>
2019-11-08 18:01:09 -0300

    Add Authorization and authorization tests

---
+++ Training1.Tests/AuthorizationServiceTests.cs
@@ -0,0 +1,390 @@
+﻿
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Training1.Authorization;
+using Training1.Models;
+using Training1.Tests.Mock;
+using Xunit;
+
+namespace Training1.Tests
+{
+    public class AuthorizationServiceTests
+    {
+        [Fact]
+        public async Task AdminAuthorization_ShowAllowProductCreateWhenAdministrator()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Create);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task ChefAuthorization_ShowAllowProductCreateWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Create);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task AccountantAuthorization_ShowAllowProductCreateWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Create);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+
+        [Fact]
+        public async Task AdminAuthorization_ShowAllowProductUpdateWhenAdministrator()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Update);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task ChefAuthorization_ShowAllowProductUpdateWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Update);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task AccountantAuthorization_ShowAllowProductUpdateWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Update);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+
+        [Fact]
+        public async Task AdminAuthorization_ShowAllowProductReadWhenAdministrator()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Read);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task ChefAuthorization_ShowAllowProductReadWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Read);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task AccountantAuthorization_ShowAllowProductReadWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Read);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+
+
+        [Fact]
+        public async Task AdminAuthorization_ShowAllowProductDeleteWhenAdministrator()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task AdminAuthorization_ShowNotAllowProductDeleteWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task AdminAuthorization_ShowNotAllowProductDeleteWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task ChefAuthorization_ShowNotAllowProductDeleteWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task ChefAuthorization_ShowNotAllowProductDeleteWhenAdmin()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task ChefAuthorization_ShowNotAllowProductDeleteWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, ChefAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task AccountantAuthorization_ShowNotAllowProductDeleteWhenChef()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserChefRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task AccountantAuthorization_ShowNotAllowProductDeleteWhenAdmin()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAdministratorsRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task AccountantAuthorization_ShowNotAllowProductDeleteWhenAccountant()
+        {
+            //Arrange
+            var authorizationService = MockAuthorizationService.BuildAuthorizationService(services =>
+            {
+                services.AddScoped<IAuthorizationHandler, AccountantAuthorizationHandler>();
+            });
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.Name, "homer.simpson"),
+                        new Claim(ClaimTypes.Role, Constants.UserAccountantRole) }));
+
+            //Act
+            var allowed = await authorizationService.AuthorizeAsync(user, It.IsAny<Product>(), UserOperations.Delete);
+
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+    }
+}
---
+++ Training1/Authorization/AdminAuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Training1.Areas.Identity.Data;
+
+namespace Training1.Authorization
+{
+    public class AdminAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                            OperationAuthorizationRequirement requirement)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            // Administrators can do anything.
+            if (context.User.IsInRole(Constants.UserAdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
