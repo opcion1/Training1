f4a092df9a2f81664a1f9a462711d5202c5c8900
opcion1 <tenken74@gmail.com>
2020-04-22 16:13:45 -0400

    Test create

---
+++ Training1.Tests/Controllers/IngredientsControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Training1.Controllers;
+using Training1.Models;
+using Training1.Tests.Mock.Services;
+using Xunit;
+
+namespace Training1.Tests.Controllers
+{
+    public class IngredientsControllerTest
+    {
+        private readonly MockIngredientService _mockService;
+        private readonly IngredientsController _controller;
+        private readonly Ingredient _testIngredient;
+        private const int _idTestIngredient = 1;
+
+        public IngredientsControllerTest()
+        {
+            _mockService = new MockIngredientService();
+            _controller = GetIngredientsController(_mockService);
+            _testIngredient = new Ingredient { Id = _idTestIngredient, FoodId = 1, ProductId = 1, Quantity = 1, UnityType = UnityType.Kilogrammes };
+        }
+
+        [Fact]
+        public async Task Create_ReturnViewResult()
+        {
+            //Arrange
+            _mockService
+                .MockCreateAsync()
+                .MockGetByIdAsync(new Ingredient());
+
+            //Act
+            var result = await _controller.Create(_testIngredient);
+
+            //Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<Ingredient>(viewResult.Value);
+            _mockService.Verify();
+        }
+
+
+        private IngredientsController GetIngredientsController(MockIngredientService mockService)
+        {
+
+            var controller = new IngredientsController(mockService.Object);
+
+            return controller;
+        }
+    }
+}
---
+++ Training1.Tests/Mock/Services/MockIngredientService.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Training1.Models;
+using Training1.Services.Interfaces;
+
+namespace Training1.Tests.Mock.Services
+{
+    public class MockIngredientService : Mock<IIngredientService>
+    {
+        public MockIngredientService()
+        {
+        }
+
+        public MockIngredientService MockCreateAsync()
+        {
+            Setup(service => service.CreateAsync(It.IsAny<Ingredient>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            return this;
+        }
+
+        public MockIngredientService MockGetByIdAsync(Ingredient ingredient)
+        {
+            Setup(service => service.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(ingredient)
+                .Verifiable();
+
+            return this;
+        }
+    }
+}
